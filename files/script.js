c12a1c4823827038b49e8a8d8f43993d4cf3c5ab
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-10-05 15:37:51 +0200

    Added blue icons. Started on JS: Array created

---
+++ files/script.js
@@ -0,0 +1,35 @@
+const projects = [
+  {
+    name: 'SmoothDay',
+    description: 'Randomized smoothie-mixer built in Javascript',
+    information:
+      'Responsive website where you can randomize a smoothierecipe, select a favourite fruit and reveal a recipe.',
+    img: './images/karin-nordkvist-smoothday.png',
+    tags: ['html5', 'css3', 'javascript'],
+  },
+  {
+    name: 'Pizzabot',
+    description: 'Chat bot built in javascript',
+    information: 'Responsive website with chat-layout to order pizza.',
+    img: './images/karin-nordkvist-pizza.png',
+    tags: ['html5', 'css3', 'javascript'],
+  },
+  {
+    name: 'News site',
+    description: 'Responsive flexbox-site built with HTML and CSS',
+    information:
+      'A website for your seasonal flowers built with Flexbox and CSS Grid.',
+    img: './images/karin-nordkvist-inseason.png',
+    tags: ['html5', 'css3'],
+  },
+  {
+    name: 'Weather app',
+    description: 'Responsive API-based weather app',
+    information:
+      'Built with HTML, CSS and Javascript, fetching data from Openweather API.',
+    img: './images/karin-nordkvist-weatherbug.png',
+    tags: ['html5', 'css3', 'javascript', 'API'],
+  },
+];
+
+console.log(projects);
